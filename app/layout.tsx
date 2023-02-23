78fca26d062178a3e1c689b3a72e0ccc414ebfa1
Jose Quijije <josequijije5@gmail.com>
2023-02-22 21:16:47 -0600

    primerc

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+"use client"
+import NavBar from "../components/NavBar"
+import { ThemeProvider } from "next-themes"
+import "../styles/globals.css"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <NavBar/>
+          {children}
+        </ThemeProvider>
+        </body>
+    </html>
+  )
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,68 @@
+"use client"
+import React, { useState} from 'react'
+import { useTheme } from 'next-themes'
+import { IoMdClose, IoMdMenu } from 'react-icons/io'
+import { RiMoonFill, RiSunLine } from 'react-icons/ri'
+import Link from 'next/link'
+
+interface navItem {
+    label: string,
+    page: string,
+}
+const Nav_Items: Array<navItem> = [
+    {
+        label: 'Home',
+        page: 'home'
+    },
+    {
+        label: 'About',
+        page: 'about'
+    },
+    {
+        label: 'Profile',
+        page: 'profile'
+    },
+]
+const NavBar = () => {
+    const [ navBar, setNavBar ] = useState(false)
+    const { theme, setTheme, systemTheme } = useTheme()
+    const currentTheme = theme ==="system" ? systemTheme : theme
+    return (
+    <header className='w-full mx-auto py-7 px-4 sm:px-20 fixed top-0 z-50 shadow  dark:border-b-2 dark:border-neutral-100' >
+        <div className="justify-between md:items-center md:flex">
+            <div>
+                <div className='mx-4 md:block'>
+                    <div >
+                        <h2>Hello, There</h2>
+                    </div>
+                    <div className='md:hidden'>
+                        <button className=' p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border' onClick={() => setNavBar(!navBar)}> 
+                            { navBar ?  <IoMdClose size={30}/> : <IoMdMenu size={30}/> }
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className={`flex-1 mx-4 md:pb-0 md:mt-0 md:block ${navBar ? "block" : "hidden" }`}>
+                    <div >
+                        <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                                { Nav_Items.map((item,idx) => {
+                                    return ( 
+                                        <Link className='block lg:inline-block  hover:text-neutral-500 dark:text-white' href={`${item.label}`} key={idx}>{item.label}</Link>
+                                    )
+                                })}
+                                { currentTheme === "dark" ? (
+                                    <button onClick={() => setTheme("Light")}><RiSunLine size={25}/></button>
+                                ) : (
+                                    <button onClick={() => setTheme("dark")}><RiMoonFill size={25}/></button>
+                                )}
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default NavBar
